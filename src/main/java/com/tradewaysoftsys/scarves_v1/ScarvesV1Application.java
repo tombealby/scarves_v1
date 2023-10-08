c69c9d86ee0c4f4e8009261b412fd065f1cde919
tombealby <tom.bealby@outlook.com>
2023-10-08 13:48:40 +0100

    Add maven project.

---
+++ src/main/java/com/tradewaysoftsys/scarves_v1/ScarvesV1Application.java
@@ -0,0 +1,13 @@
+package com.tradewaysoftsys.scarves_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScarvesV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScarvesV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradewaysoftsys/scarves_v1/ScarvesV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradewaysoftsys.scarves_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScarvesV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
